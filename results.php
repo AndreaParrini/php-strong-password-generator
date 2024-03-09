6faa5fe5ee75f402005ecd7f508c134a285e77c8
AndreaParrini <andrea.parrini26@gmail.com>
2024-03-09 16:36:37 +0100

    add style to div of show password

--- results.php
+++ results.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 
-echo $_SESSION['password'];
-
 if (isset($_SESSION['password'])) {
     $password = $_SESSION['password'];
 }
@@ -10,6 +8,22 @@ if (isset($_SESSION['password'])) {
 
 <?php include __DIR__ . '/header.php' ?>
 
-</body>
+<div class="container">
+    <div class="bg-light p-2">
+        <div class="p-5 d-flex align-items-center gap-3">
+            <div class="fs-5">
+                Password :
+            </div>
+            <div class="badge text-bg-primary p-3 fs-5">
+                <?= $password ?>
+            </div>
+        </div>
+        <div class="ps-5 pb-2">
+            <a href="./index.php" class="btn btn-warning">Torna al Generator</a>
+        </div>
 
-</html>
+    </div>
+
+</div>
+
+<?php include __DIR__ . '/footer.php' ?>
